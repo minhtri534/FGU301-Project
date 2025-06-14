2d075f0f75780230fb7c7778768489d089f2900f
NguyenVoManhHung <usernameabc124@gmail.com>
2025-06-14 15:08:39 +0700

    Added acceleration to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float move_speed = 6;
-    public float jump_force = 6;
+    public float max_move_speed = 6;
+    public float acceleration = 25;
+    public float jump_force = 9;
     public Animator anim;
     private Rigidbody2D rb;
     public ContactFilter2D ContactFilter;
@@ -14,8 +16,6 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        move_speed = 6;
-        jump_force = 9;
         rb = GetComponent<Rigidbody2D>();
         move = InputSystem.actions.FindAction("Move");
         jump = InputSystem.actions.FindAction("Jump");
@@ -24,19 +24,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocityX = move_speed * move.ReadValue<float>();
+        float move_value = move.ReadValue<float>();
+        // Increase speed by acceleration until max speed
+        if (Mathf.Abs(rb.linearVelocityX) < max_move_speed)
+        {
+            rb.linearVelocityX += acceleration * move_value * Time.deltaTime;
+        }
+        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -max_move_speed, max_move_speed);
+        if (move_value == 0)
+        {
+            rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, 0, acceleration * Time.deltaTime);
+        }
+        
+
         // Jumping
         if (jump.WasPressedThisFrame() && IsGrounded())
         {
             rb.linearVelocityY = jump_force;
         }
+
         // Flip model
         if (move.ReadValue<float>() != 0)
         {
-            transform.localScale = new Vector3(10 * move.ReadValue<float>(), 10, 1);
+            transform.localScale = new Vector3(move_value, 1, 1);
         }
         // Animation(cần có trị tuyệt đối do đk là lớn hơn 0.1 thì player run)
-        anim.SetFloat("move", Mathf.Abs(move.ReadValue<float>()));
+        anim.SetFloat("move", Mathf.Abs(move_value));
 
         //  (chưa xong)
         anim.SetBool("jump", !IsGrounded());
