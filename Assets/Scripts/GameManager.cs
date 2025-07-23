7c464c04eea442146326276da1d44924d8afdd47
minhtri534 <104714507+minhtri534@users.noreply.github.com>
2025-07-23 07:21:27 +0700

    UI key

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
     void Start()
     {
-        
+        UpdateScore();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = score.ToString();
     }
 }
