3153b5b3ccca20843b81863a65a2c8da9a846f3e
minhtri534 <104714507+minhtri534@users.noreply.github.com>
2025-07-23 09:37:12 +0700

    Add UI Menu, GameWin, GameOver

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private int score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private GameObject gameWinUI;
+    private bool isGameOver = false;
+    private bool isGameWin = false;
     void Start()
     {
         UpdateScore();
+        gameOverUI.SetActive(false);
+        gameWinUI.SetActive(false);
     }
 
     void Update()
@@ -17,12 +24,56 @@ public class GameManager : MonoBehaviour
 
     public void AddScore(int points)
     {
-        score += points;
-        UpdateScore();
+        if (!isGameOver && !isGameWin)
+        {
+            score += points;
+            UpdateScore();
+        }
+
     }
 
     private void UpdateScore()
     {
         scoreText.text = score.ToString();
     }
+    public void GameOver()
+    {
+        isGameOver = true;
+        score = 0;
+        Time.timeScale = 0;
+        gameOverUI.SetActive(true);
+    }
+
+    public void GameWin()
+    {
+        isGameWin = true;
+        Time.timeScale = 0;
+        gameWinUI.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        isGameOver = false;
+        score = 0;
+        UpdateScore();
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1;
+    }
+
+    public bool IsGameOver()
+    {
+        return isGameOver;
+    }
+    
+    public bool IsGameWin()
+    {
+        return isGameWin;
+    }    
 }
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
